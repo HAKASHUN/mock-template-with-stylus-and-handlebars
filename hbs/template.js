096113bb473d2ed7fdf299ecaede0e47f1705e23
Shunsuke Hakamata <hakashun@hakashun.net>
2013-10-22 10:51:45 +0900

    mockテンプレートファイルを修正

--- hbs/template.js
+++ hbs/template.js
@@ -7,6 +7,6 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   var buffer = "";
 
 
-  buffer += "\n\n<h1>hogehoge</h1>\n<ul>\n    <li>hoge</li>\n    <li>hoge</li>\n    <li>hoge</li>\n    <li>hoge</li>\n    <li>hoge</li>\n    <li>hoge</li>\n    <li>hoge</li>\n    <li>hoge</li>\n    <li>hoge</li>\n</ul>\n\n\n";
+  buffer += "\n\n<header>\n    hoge\n</header>\n\n<section>\n    hogehoge\n</section>\n\n<footer>\n    hoges\n</footer>";
   return buffer;
   });
